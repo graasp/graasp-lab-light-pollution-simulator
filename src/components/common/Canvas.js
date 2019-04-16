9491c0aed25bc0ec701fa7016906b8059803994e
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2019-04-16 14:25:54 +0200

    feat: draw tree in the canvas
    Draw a tree in the bottom right of the canvas.
    
    closes #9

--- src/components/common/Canvas.js
+++ src/components/common/Canvas.js
@@ -1,37 +1,76 @@
 import React, { Component } from 'react';
-import { Stage, Layer, Rect, Text } from 'react-konva';
-import Konva from 'konva';
+import { Stage, Layer } from 'react-konva';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Tree from '../svgs/Tree';
 
 class Canvas extends Component {
-  state = {
-    color: 'green',
+  static styles = {
+    canvas: {
+      backgroundColor: 'white',
+    },
   };
 
-  handleClick = () => {
-    this.setState({
-      color: Konva.Util.getRandomColor(),
-    });
+  state = {
+    height: window.innerHeight,
+    width: window.innerWidth,
+    tree: {
+      x: 100,
+      y: 100,
+      scaleX: 0.25,
+      scaleY: 0.25,
+    },
+  };
+
+  static propTypes = {
+    classes: PropTypes.shape({}).isRequired,
+  };
+
+  componentDidMount() {
+    const scale = this.calculateScale();
+    const { tree, height, width } = this.state;
+    const newTree = {
+      ...tree,
+      scaleX: scale,
+      scaleY: scale,
+      y: height - 350,
+      x: width - 500,
+    };
+
+    this.setState({ tree: newTree });
+  }
+
+  /**
+   * This is a sample way of calculating the scale to be used by objects
+   * using only the height of the window.
+   * @returns {number}
+   */
+  calculateScale = () => {
+    const { height } = this.state;
+    const factor = 250;
+    return factor / height;
   };
 
   render() {
-    const { color } = this.state;
+    const { tree } = this.state;
+
+    const { classes } = this.props;
+
     return (
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          <Text text="Try click on rect" />
-          <Rect
-            x={20}
-            y={20}
-            width={50}
-            height={50}
-            fill={color}
-            shadowBlur={5}
-            onClick={this.handleClick}
-          />
-        </Layer>
-      </Stage>
+      <div className={classes.canvas}>
+        <Stage width={window.innerWidth} height={window.innerHeight - 100}>
+          <Layer>
+            <Tree
+              x={tree.x}
+              y={tree.y}
+              scaleX={tree.scaleX}
+              scaleY={tree.scaleY}
+            />
+          </Layer>
+        </Stage>
+      </div>
     );
   }
 }
 
-export default Canvas;
+export default withStyles(Canvas.styles)(Canvas);
